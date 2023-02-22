056d2077dadc78459e8d7bfd10982c5a9f9ce580
odinzv69 <125555046+odinzv69@users.noreply.github.com>
2023-02-22 13:54:43 +0300

    yes

---
+++ script.js
@@ -0,0 +1,47 @@
+let a = 20;
+alert(a);
+
+let firstIphone = 2007;
+alert(firstIphone);
+
+let creatorJavascript = "Brendan Eich";
+alert(creatorJavascript);
+
+let x = 10;
+let y = 2;
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
+
+let result = 2**5;
+alert(result);
+
+let A = 9;
+let b = 2;
+alert(A%b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('сколько вам лет?');
+alert(age);
+
+const user = {
+    name: String('Bob'),
+    age: Number(19),
+    isAdmin: Boolean(true),
+}
+user.cityOfResidence = String('Moscow');
+
+let info = prompt('Какую информацию хотите узнать о пользователе?', 'user');
+alert(user[info]);
+
+let admin= String(prompt('Введите ваше имя'));
+ alert(`Привет, ${admin} !`);
